dcec54abaaeaec3f94b61a378f2cab1a34b77dd5
root <NyangoyaG>
2023-09-28 19:55:43 +0000

    1. Let's make some classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,15 +1,9 @@
-// Import the ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-// Implement the initializeRooms function
-function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34),
+  ];
 }
-
-// Usage example
-const rooms = initializeRooms();
-console.log(rooms); // This will print the array of ClassRoom objects to the console
