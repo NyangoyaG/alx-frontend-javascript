d9048edee16b88f8ca016d67eb0967b15929e1be
root <NyangoyaG>
2023-10-06 10:47:07 +0000

    7-has_array_values.js

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,2 +1,12 @@
-export default function hasValuesFromArray(set, arr) {
-  return arr.reduce((prev, curr) => prev && set.has(curr), true);
+export default function hasValuesFromArray(set, array) {
+  // Use the every() method to check if all elements in the array exist in the set
+  return array.every((element) => set.has(element));
+}
+
+// Example usage:
+const mySet = new Set([1, 2, 3, 4, 5]);
+const myArray1 = [1, 2, 3];
+const myArray2 = [3, 4, 6];
+
+console.log(hasValuesFromArray(mySet, myArray1)); // true, all elements in myArray1 exist in mySet
+console.log(hasValuesFromArray(mySet, myArray2)); // false, not all elements in myArray2 exist in mySet	
